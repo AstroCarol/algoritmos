48f0ef7350e0085751206ef6925e77c8b4c0f4dd
Astrogilda Caroline <astro.carol.gil@gmail.com>
2017-09-12 18:22:55 -0300

    Update and rename conceito.c to 11.c

--- Lista 1 - Condicional/11.c
+++ Lista 1 - Condicional/11.c
@@ -1,3 +1,11 @@
+/* Fazer um algoritmo para imprimir o conceito de um aluno, dada a sua nota. Supor notas inteiras somente.
+O critério para conceitos é o seguinte:
+ nota inferiores a 3 - conceito E
+ nota de 3 a 5 - conceito D
+ notas 6 e 7 - conceito C
+ notas 8 e 9 - conceito B
+nota 10 - conceito A*/
+
 #include <stdio.h>
 
 int main(int argc, char const *argv[])
