c2e6c74b11610efde110713ddecf206acd66d507
Astrogilda Caroline <astro.carol.gil@gmail.com>
2017-09-12 18:21:06 -0300

    Update and rename trian.c to 9.c

--- Lista 1 - Condicional/9.c
+++ Lista 1 - Condicional/9.c
@@ -1,3 +1,7 @@
+/*Dados três valores X, Y e Z, verificar se eles podem ser os comprimentos dos lados de um triângulo e, se
+forem, verificar se é um triângulo eqüilátero, isósceles ou escaleno. Se eles não formarem um triângulo,
+escreva uma mensagem.*/
+
 #include <stdio.h>
 
 int triangulo(int a, int b, int c)
